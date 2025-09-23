76fb48135dc62bde9d29daade36afc108f5bc621
jaylineweb <darkjay118@gmail.com>
2025-09-23 13:06:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Macsumsuk landing page loaded.");
+(function ($) {
+    $(function () {
+        var $heroSlider = $(".hero-slider");
+
+        if ($heroSlider.length) {
+            $heroSlider.slick({
+                dots: true,
+                arrows: true,
+                autoplay: true,
+                autoplaySpeed: 6000,
+                speed: 800,
+                fade: true,
+                cssEase: "ease-in-out",
+                pauseOnHover: true,
+                adaptiveHeight: false
+            });
+        }
+    });
+})(jQuery);
